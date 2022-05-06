7c0c07675023d6dcb8d7accdaf5df90a8478a925
anachioca <ana.chioca@gmail.com>
2022-05-06 11:29:49 -0300

    Unit Tests - MessageTest

---
+++ src/MessageTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+public class MessageTest {
+}
